55bcd6c5cf28f80db46012c2e023eafcb2ef8fc8
getuser09 <75838586+getuser09@users.noreply.github.com>
2020-12-11 13:41:30 +0100

    fd

--- app/ch/moldova/moldova.php
+++ app/ch/moldova/moldova.php
@@ -10,6 +10,6 @@
 "kategory":"Channels",
 "ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/moldova.png",
 "ulke":"Moldova",
-"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/moldova/channel_moldova.txt",
+"cat_url":"https://github.com/getuser09/file/raw/master/app/ch/moldova/channel_moldova.txt",
 "cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
 ]}
